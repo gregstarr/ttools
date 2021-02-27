b57a994847597375095da768433512cb89669346
Greg Starr <gregstarr2@gmail.com>
2021-02-26 21:50:26 -0400

    fixing google drive file path, reworking config

--- test/conftest.py
+++ test/conftest.py
@@ -2,22 +2,26 @@ import pytest
 import os
 
 
-@pytest.fixture
-def madrigal_data_dir():
-    yield os.path.join(os.path.dirname(__file__), 'test_madrigal_data')
+@pytest.fixture()
+def test_data_dir():
+    yield os.path.join(os.path.dirname(__file__), 'ttools_data')
 
 
 @pytest.fixture
-def tec_data_dir():
-    yield os.path.join(os.path.dirname(__file__), 'test_tec_data')
+def madrigal_data_dir(test_data_dir):
+    yield os.path.join(test_data_dir, 'madrigal')
 
 
 @pytest.fixture
-def swarm_data_dir():
-    yield os.path.join(os.path.dirname(__file__), 'test_swarm_data')
+def tec_data_dir(test_data_dir):
+    yield os.path.join(test_data_dir, 'tec_data')
 
 
 @pytest.fixture
-def swarm_data_fn():
-    yield os.path.join(os.path.dirname(__file__), 'test_swarm_data',
-                       "SW_EXTD_EFIC_LP_HM_20191226T000000_20191226T235959_0102.cdf")
+def swarm_data_dir(test_data_dir):
+    yield os.path.join(test_data_dir, 'swarm')
+
+
+@pytest.fixture
+def swarm_data_dir(test_data_dir):
+    yield os.path.join(test_data_dir, 'swarm')
--- ttools/config.py
+++ ttools/config.py
@@ -3,28 +3,39 @@ import h5py
 import numpy as np
 
 
-# madrigal TEC data directory
-madrigal_dir = "E:\\tec_data\\download"
-madrigal_lat = np.arange(-90, 90)
-madrigal_lon = np.arange(-180, 180)
+def update(_base_dir="E:\\"):
+    global base_dir, madrigal_dir, madrigal_lat, madrigal_lon, tec_dir, arb_dir, swarm_dir, swarm_coords_dir
+    global kp_file, mlt_grid, mlat_grid, PARALLEL
+    base_dir = _base_dir
 
-# processed TEC data directory
-tec_dir = "E:\\tec_data"
+    # madrigal TEC data directory
+    madrigal_dir = os.path.join(base_dir, "tec_data", "download")
+    madrigal_lat = np.arange(-90, 90)
+    madrigal_lon = np.arange(-180, 180)
 
-# processed auroral boundary data directory
-arb_dir = "E:\\auroral_boundary"
+    # processed TEC data directory
+    tec_dir = os.path.join(base_dir, "tec_data")
 
-# SWARM data directory
-swarm_dir = "E:\\swarm\\extracted"
-swarm_coords_dir = "E:\\swarm\\coordinates"
+    # processed auroral boundary data directory
+    arb_dir = os.path.join(base_dir, "auroral_boundary")
 
-grid_file = os.path.join(os.path.dirname(__file__), "grid.h5")
-with h5py.File(grid_file, 'r') as f:
-    mlt_vals = f['mlt'][()]
-    mlat_vals = f['mlat'][()]
-mlt_grid, mlat_grid = np.meshgrid(mlt_vals, mlat_vals)
+    # SWARM data directory
+    swarm_dir = os.path.join(base_dir, "swarm", "extracted")
+    swarm_coords_dir = os.path.join(base_dir, "swarm", "coordinates")
 
-kp_file = os.path.join(os.path.dirname(__file__), "2000_2020_kp_ap.txt")
+    grid_file = os.path.join(tec_dir, "grid.h5")
+    if os.path.exists(grid_file):
+        with h5py.File(grid_file, 'r') as f:
+            mlt_vals = f['mlt'][()]
+            mlat_vals = f['mlat'][()]
+        mlt_grid, mlat_grid = np.meshgrid(mlt_vals, mlat_vals)
+    else:
+        print("GRID FILE NOT FOUND")
 
-# other settings
-PARALLEL = True
+    kp_file = os.path.join(base_dir, "2000_2020_kp_ap.txt")
+
+    # other settings
+    PARALLEL = True
+
+
+update()
